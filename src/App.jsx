d9bf6d27edd9ca725e38bef435b289da78c1baa6
satyam18x <satyamhaldkar45@gmail.com>
2026-07-19 12:50:20 +0530

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,9 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React from 'react'
 
-function App() {
-
-  const [seconds, setSeconds] = useState(0);
-  const [isRunning, SetisRunning] = useState(false);
-  
-
-  useEffect(() => {
-    let interval;
-
-    if (isRunning) {
-      interval = setInterval(() => {
-        setSeconds((prev) => prev + 1);
-      }, 1000)
-    }
-
-    return () => clearInterval(interval);
-
-  }, [isRunning]);
-
-  
-
+const App = () => {
   return (
-    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-      <h1>Timer App</h1>
-      <h1>Count : {seconds}</h1>
-      <div style={{ display: "flex", gap: "15px" }}>
-
-        <button style={{ background: "black", color: "white", padding: "10px", width: "60px" }}
-          onClick={() => { SetisRunning(true) }}>Start</button>
-
-        <button style={{ background: "black", color: "white", padding: "10px", width: "60px" }}
-          onClick={() => { SetisRunning(false) }}>Stop</button>
-
-        <button style={{ background: "black", color: "white", padding: "10px", width: "60px" }}
-          onClick={() => {
-            SetisRunning(false)
-            setSeconds(0);
-          }}>Reset</button>
-
-      </div> 
-             { isRunning && <h1>Timer is Active</h1>}
-      
-            
-    </div>
-  );
-
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
